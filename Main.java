f6b530679b482c7920972651a1b726c17ce1d356
Mikables <108348416+Mikables@users.noreply.github.com>
2026-06-09 11:26:43 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, GOALS in CS!.");
+        System.out.println("Hello, GOALS in CS!");
     }
 }
